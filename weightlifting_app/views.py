4c82c7a7e4139d4f557e5112c4452a28636cce39
Alex Smith <a.smithycchs@gmail.com>
2017-07-25 14:38:51 +0200

    added simple view for sign in page

--- weightlifting_app/views.py
+++ weightlifting_app/views.py
@@ -8,13 +8,14 @@ def create_program(request):
     return render(request, 'create_program.html')
 
 def submit_form(request):
-    print("here")
     if request.method == 'POST':
         print("received form!")
-        print (request.POST['gender'])
         
     return render(request, 'form_values.html', {'form' : request.POST})
 
 def lift_analyser(request):
     return render(request, 'lift_analyser.html')
-# Create your views here.
+
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
